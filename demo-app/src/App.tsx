0dbd5b166026553e332c203ae1dffbe63e8fe390
Hussain Pettiwala <hussain.pettiwala@gmail.com>
2021-11-01 18:50:36 +0530

    ⚡ NEW: example

--- demo-app/src/App.tsx
+++ demo-app/src/App.tsx
@@ -7,22 +7,23 @@ function App() {
   const navContainerRef = useRef<HTMLDivElement | null>(null);
 
   return (
-    <div style={{}}>
+    <div>
       <Navigation />
 
-      <div style={{}}>
-        <ScrollSpy>
-          <Center id="orange" backgroundColor={"orange"}>
-            <h1>Hello World</h1>
-          </Center>
-          <Center id="blue" backgroundColor={"blue"}>
-            <h1>Hello World</h1>
-          </Center>
-          <Center id="green" backgroundColor={"green"}>
-            <h1>Hello World</h1>
-          </Center>
-        </ScrollSpy>
-      </div>
+      <ScrollSpy scrollThrottle={100}>
+        <Center id="orange" backgroundColor={"orange"}>
+          <h1>Orange</h1>
+        </Center>
+        <Center id="brown" backgroundColor={"brown"}>
+          <h1>Brown</h1>
+        </Center>
+        <Center id="blue" backgroundColor={"blue"}>
+          <h1>Blue</h1>
+        </Center>
+        <Center id="green" backgroundColor={"green"}>
+          <h1>Green</h1>
+        </Center>
+      </ScrollSpy>
     </div>
   );
 }
--- demo-app/src/components/Navigation/Navigation.tsx
+++ demo-app/src/components/Navigation/Navigation.tsx
@@ -4,17 +4,39 @@ interface Props {}
 
 const Navigation = (props: Props) => {
   return (
-    <div>
-      <div style={{ position: "fixed" }}>
-        <div data-to-scrollspy-id="orange" className={"ss-item"}>
-          orange
-        </div>
-        <div data-to-scrollspy-id="blue" className={"ss-item"}>
-          blue
-        </div>
-        <div data-to-scrollspy-id="green" className={"ss-item"}>
-          green
-        </div>
+    <div
+      style={{
+        position: "fixed",
+        backgroundColor: "beige",
+        width: "100%",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <a href="#orange">
+          <div data-to-scrollspy-id="orange" className={"ss-item"}>
+            orange
+          </div>
+        </a>
+        <a href="#brown">
+          <div data-to-scrollspy-id="brown" className={"ss-item"}>
+            brown
+          </div>
+        </a>
+        <a href="#blue">
+          <div data-to-scrollspy-id="blue" className={"ss-item"}>
+            blue
+          </div>
+        </a>
+        <a href="#green">
+          <div data-to-scrollspy-id="green" className={"ss-item"}>
+            green
+          </div>
+        </a>
       </div>
     </div>
   );
